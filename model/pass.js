0ef17471bfa7f1a86b4b9d701b56a5ad839eb4d7
Tharun Vemula <tharunvind@gmail.com>
2022-11-05 15:59:59 +0530

    db models

---
+++ model/pass.js
@@ -0,0 +1,52 @@
+const { Timestamp } = require('bson');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const passSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    rollNumber: {
+      type: String,
+      required: true,
+    },
+    Date: {
+      type: Date,
+      required: true,
+    },
+    outTime: {
+      type: Timestamp,
+      required: true,
+    },
+
+    inTime: {
+      type: Timestamp,
+      required: true,
+    },
+    purpose: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    inKey: {
+      type: String,
+      required: true,
+    },
+    outKey: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model('Pass', passSchema);
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const bodyParser = require('body-parser');
+
+const MONGO_URI =
+  'mongodb://tarun:ktg2bFu8b3RMsKSf@cluster0-shard-00-00.iy4kk.mongodb.net:27017,cluster0-shard-00-01.iy4kk.mongodb.net:27017,cluster0-shard-00-02.iy4kk.mongodb.net:27017/sample?ssl=true&replicaSet=atlas-k2o103-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 3000;
+
+const authRouter = require('./routes/auth');
+const passRouter = require('./routes/pass');
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Math.floor(Math.random() * 1000) + '-' + file.originalname);
+  },
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer({ storage: fileStorage }).single('file'));
+
+// app.use(authRouter);
+// app.use(passRouter);
+
+mongoose
+  .connect(MONGO_URI)
+  .then((result) => {
+    app.listen(PORT, (req, res) => {
+      console.log('Running');
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
